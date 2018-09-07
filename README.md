89518bcb41045e2d7c096935d1aff6a6cd67cf81
İbrahim Kahraman <ibrahim.kahraman@iyzico.com>
2018-09-07 09:54:16 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## About
+
+## Tech Stack
+Tech|Version
+---|---
+JDK|1.8
---
+++ src/main/java/com/kahramani/p2p/Application.java
@@ -0,0 +1,15 @@
+package com.kahramani.p2p;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    private static final int APPLICATION_DEFAULT_PORT = 8080;
+
+    public static void main(String[] args) {
+
+    }
+}
